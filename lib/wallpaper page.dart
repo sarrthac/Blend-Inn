09ebc93b81cc752be5ddb76ce52452a792b3a8f5
Sarrthac <sarthakzalte@gmail.com>
2021-08-06 02:25:13 +0530

    "Added load more button and updated its functionalities"

--- lib/wallpaper page.dart
+++ lib/wallpaper page.dart
@@ -13,6 +13,7 @@ class wallP_Screen extends StatefulWidget {
 class _wallP_ScreenState extends State<wallP_Screen> {
 
   List images = [];
+  int page = 1;
   @override
   void initState(){
     super.initState();
@@ -29,6 +30,21 @@ class _wallP_ScreenState extends State<wallP_Screen> {
       print(images);
     });
   }
+
+  loadmoreimages() async{
+    setState(() {
+      page = page + 1;
+    });
+    String url = 'https://api.pexels.com/v1/curated?per_page=80&page='+page.toString();
+    await http.get(Uri.parse(url),
+        headers:{'Authorization':'563492ad6f91700001000001dff3a71159854dc7868ce269de803373'
+        }).then((value){
+          Map result = jsonDecode(value.body);
+          setState(() {
+            images.addAll(result['photos']);
+          });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,31 +54,39 @@ class _wallP_ScreenState extends State<wallP_Screen> {
       body: Column(
         children: [
           Expanded(child: Container(
-          child: GridView.builder( itemCount: 80,
+          child: GridView.builder( itemCount: images.length,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisSpacing: 2.0,
                 crossAxisCount: 3,
                 childAspectRatio: 2/3,
                 mainAxisSpacing: 2,
               ), itemBuilder: (context,index){
-            return Container(color: Colors.white,);
+            return Container(color: Colors.white,
+            child: Image.network(images[index]['src']['tiny'],
+            fit: BoxFit.cover,),
+            );
               }),
           ),
           ),
-          Container(
-            //margin: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.black
+          InkWell(
+            onTap: (){
+              loadmoreimages();
+            },
+            child: Container(
+              //margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.black
+              ),
+              height: 60,
+              width: double.infinity,
+              child: Center(
+                child: Text('Load More',
+            style: TextStyle(
+                fontSize: 20,
             ),
-            height: 60,
-            width: double.infinity,
-            child: Center(
-              child: Text('Load More',
-          style: TextStyle(
-              fontSize: 20,
-          ),
-          ),
+            ),
+              ),
             ),
           ),
         ],
