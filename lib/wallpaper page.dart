fa4819491aee805696df93a3d75ecc60a3e11583
Sarrthac <sarthakzalte@gmail.com>
2021-08-05 23:51:22 +0530

    "Fetched the API data and done with basic grid outline"

--- lib/wallpaper page.dart
+++ lib/wallpaper page.dart
@@ -1,13 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class wallP_Screen extends StatefulWidget {
-  const wallP_Screen({Key? key}) : super(key: key);
+
 
   @override
   _wallP_ScreenState createState() => _wallP_ScreenState();
 }
 
 class _wallP_ScreenState extends State<wallP_Screen> {
+
+  List images = [];
+  @override
+  void initState(){
+    super.initState();
+    fetchapi();
+  }
+  fetchapi()async{
+    await http.get(Uri.parse('https://api.pexels.com/v1/curated?per_page=80'),
+        headers:{'Authorization':'563492ad6f91700001000001dff3a71159854dc7868ce269de803373'
+    }).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images = result['photos'];
+      });
+      print(images);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +37,20 @@ class _wallP_ScreenState extends State<wallP_Screen> {
       ),
       body: Column(
         children: [
-          Expanded(child: Container()),
+          Expanded(child: Container(
+          child: GridView.builder( itemCount: 80,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 2.0,
+                crossAxisCount: 3,
+                childAspectRatio: 2/3,
+                mainAxisSpacing: 2,
+              ), itemBuilder: (context,index){
+            return Container(color: Colors.white,);
+              }),
+          ),
+          ),
           Container(
-            margin: EdgeInsets.all(15),
+            //margin: EdgeInsets.all(15),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               color: Colors.black
